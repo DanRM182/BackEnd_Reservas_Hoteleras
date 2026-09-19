833f39e650e618d2be869b2db33935a371cabe6b
DanRM182 <ddaannn182@gmail.com>
2026-09-18 21:06:23 -0600

    Commons, autenticación, configuración de Eureka y Gateway

---
+++ commons/src/main/java/com/dan/commons/utils/FunctionUtils.java
@@ -0,0 +1,12 @@
+package com.dan.commons.utils;
+
+import com.dan.commons.exceptions.EntidadRelacionadaException;
+import com.dan.commons.exceptions.RecursoNoEncontradoException;
+import feign.FeignException;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class FunctionUtils {
+
+}
