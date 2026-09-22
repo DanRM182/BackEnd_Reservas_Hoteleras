297ba008b4e171f1a0bf2e49bf5cbc085ffb8f31
DanRM182 <ddaannn182@gmail.com>
2026-09-22 11:01:27 -0600

    DTOs y entidad de la reserva concluídos

---
+++ reservas/src/main/java/com/reservasHotel/reservas/mapper/ReservaMapper.java
@@ -0,0 +1,17 @@
+package com.reservasHotel.reservas.mapper;
+
+import com.reservasHotel.commons.mapper.CommonMapper;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReservaMapper implements CommonMapper {
+    @Override
+    public Object requestAEntidad(Object request) {
+        return null;
+    }
+
+    @Override
+    public Object entidadAResponse(Object entidad) {
+        return null;
+    }
+}
