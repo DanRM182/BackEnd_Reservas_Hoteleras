593d5d9e962c7e63efbd7317f11774a11a1c6c48
DanRM182 <ddaannn182@gmail.com>
2026-09-22 11:46:53 -0600

    Mapper de reserva y método de crear finalizados

--- reservas/src/main/java/com/reservasHotel/reservas/mapper/ReservaMapper.java
+++ reservas/src/main/java/com/reservasHotel/reservas/mapper/ReservaMapper.java
@@ -1,17 +1,31 @@
 package com.reservasHotel.reservas.mapper;
 
 import com.reservasHotel.commons.mapper.CommonMapper;
+import com.reservasHotel.reservas.dto.ReservaRequest;
+import com.reservasHotel.reservas.dto.ReservaResponse;
+import com.reservasHotel.reservas.entity.Reserva;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ReservaMapper implements CommonMapper {
+public class ReservaMapper implements CommonMapper<ReservaRequest, ReservaResponse, Reserva> {
     @Override
-    public Object requestAEntidad(Object request) {
-        return null;
+    public Reserva requestAEntidad(ReservaRequest request) {
+        return request != null ?
+                Reserva.crear(
+                        request.idHabitacion(),
+                        request.idHuesped(),
+                        request.fechaEntrada(),
+                        request.fechaSalida()) : null;
     }
 
     @Override
-    public Object entidadAResponse(Object entidad) {
-        return null;
+    public ReservaResponse entidadAResponse(Reserva entidad) {
+        return entidad != null ?
+                new ReservaResponse(
+                        entidad.getId(),
+                        null, null,
+                        entidad.getEstadoReserva().getDescripcion(),
+                        entidad.getFechaEntrada(),
+                        entidad.getFechaSalida()) : null;
     }
 }
